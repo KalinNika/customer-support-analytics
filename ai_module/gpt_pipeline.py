355d75ee4e662a8f9cf0e8d16b18dd7fc0370176
Nika Kalinnikova <NicaWilliams@yandex.ru>
2025-06-23 19:06:06 +0300

    gpt_pipeline.py

--- ai_module/gpt_pipeline.py
+++ ai_module/gpt_pipeline.py
@@ -1,11 +1,2 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("client_requests.csv")
-df_count = df.groupby("date").size().reset_index(name="y")
-
-plt.plot(df_count["date"], df_count["y"], marker="o")
-plt.title("Requests by Date")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+# 📄 View the resulting report (PDF):
+# https://github.com/KalinNika/customer-support-analytics/blob/main/weekly_report.pdf
